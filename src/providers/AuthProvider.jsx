0bd40290fe8be6786de5089e9a06f3e6426c3112
sal400 <flarebd@gmail.com>
2024-03-13 03:24:11 +0600

    AuthProvider completed

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,9 +1,54 @@
-import { createContext } from "react";
+import { createContext } from 'react';
+import { GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import { useState, useEffect } from 'react';
+import auth from '../firebase/firebase.config';
 
-export const AuthContext = createContext(null) 
+export const AuthContext = createContext(null)
+const googleProvider = new GoogleAuthProvider()
 
-const AuthProvider = ({children}) => {
-    const authInfo = {name: 'auth info here'}
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    //Manage User
+    useEffect(()=>{
+        const unSubscribe = onAuthStateChanged(auth, (currentUser)=>{
+            setUser(currentUser)
+            setLoading(false)
+        })
+
+        return ()=>{
+            unSubscribe()
+        }
+
+    }, [])
+
+    //Google -> Sign in
+    const signinGoogle=()=>{
+        setLoading(true)
+        return signInWithPopup(auth, googleProvider)
+    }
+
+    //Email Password -> sign up
+    const createEmail =(email, password)=>{
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    //Email Password -> Sign in
+    const signinEmail =(email, password)=>{
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+    
+    //Any method -> Logout
+    const logOut =()=>{
+        setLoading(true)
+        return signOut(auth)
+    }
+        
+
+    const authInfo = { name:'auth info here', user, loading, signinGoogle, createEmail, signinEmail, logOut }
 
     return (
         <div>
