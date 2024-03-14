1e13273f95804d547656c3bb95a594b8377c3950
sal400 <flarebd@gmail.com>
2024-03-14 09:13:40 +0600

    Page title section designed

--- src/pages/Listings.jsx
+++ src/pages/Listings.jsx
@@ -1,7 +1,15 @@
 const Listings = () => {
     return (
-        <div>
-            All listing
+        <div className="border rounded-lg">
+            <div className="text-center p-10 banner-bg rounded-t-lg uppercase">
+                <h2 className="prim font-bold text-2xl md:text-4xl">Property Listings</h2>
+                <p className="font-medium">Home / Listings</p>
+            </div>
+            <div className="pb-10 md:pb-20 px-10 md:px-20">
+                <div className="overflow-x-auto">
+                    content will be here
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,7 +1,15 @@
 const Wishlist = () => {
     return (
-        <div>
-            Wishlist
+        <div className="border rounded-lg">
+            <div className="text-center p-10 banner-bg rounded-t-lg uppercase">
+                <h2 className="prim font-bold text-2xl md:text-4xl">My Wishlist</h2>
+                <p className="font-medium">Home / Wishlist</p>
+            </div>
+            <div className="pb-10 md:pb-20 px-10 md:px-20">
+                <div className="overflow-x-auto">
+                    content will be here
+                </div>
+            </div>
         </div>
     );
 };
