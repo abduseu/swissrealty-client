d2278a54aaf39a343f06abb0ad845c69ed75a3f6
sal400 <flarebd@gmail.com>
2024-03-15 21:59:34 +0600

    Agent Listings implemented

--- src/pages/agent/AgentListings.jsx
+++ src/pages/agent/AgentListings.jsx
@@ -1,15 +1,71 @@
+import { useEffect, useState } from "react";
+import useAuth from "../../hooks/useAuth";
 import AgentDrawer from "./AgentDrawer";
+import { axiosBase } from "../../hooks/useAxios";
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const AgentListings = () => {
+    const { user } = useAuth()
+    const [listings, setListings] = useState([]);
+
+    //fetch data
+    const filterEmail = user.email;
+    useEffect(() => {
+        axiosBase(`/agent-listings?email=${filterEmail}`)
+            .then(res => {
+                console.log(res.data);
+                setListings(res.data);
+            });
+    }, [filterEmail]);
+
+
+    //Delete function
+    const handleDeleteProperty = (id) => {
+        axiosBase.delete(`/properties/${id}`)
+            .then(res => {
+                console.log(res.data);
+                if (res.data.deletedCount > 0) {
+                    Swal.fire(
+                        'Deleted!',
+                        'Property removed from listings.',
+                        'success'
+                    );
+                    const remaining = listings.filter(x => x._id !== id);
+                    setListings(remaining);
+                }
+            });
+
+        console.log('clicked: ', id);
+    };
+
+
     return (
-        <div className="md:flex">
+        <div className="md:flex border rounded-lg">
             <AgentDrawer></AgentDrawer>
             <div className="bg-cover bg-center w-full">
                 <div className="flex justify-center items-center">
                     <div className="text-center p-6 md:p-10 w-full">
                         <h3 className="text-3xl font-semibold mb-6 uppercase">Agent Listings</h3>
                         <div className="text-xl space-y-4">
-                            Content will be here
+                            <div>
+                                {listings.map((x, index) => (
+                                    <div key={x._id} className="flex justify-between items-center my-4 md:w-3/4 mx-auto">
+                                        <div className="md:flex items-center">
+                                            <div className="text-left">
+                                                <Link to={`/details/${x._id}`}>
+                                                    <h3 className="font-semibold text-sm">{index + 1}. {x.title}</h3>
+                                                    <p className="text-sm">{x.location}</p>
+                                                </Link>
+                                            </div>
+                                        </div>
+                                        <div className="flex items-center gap-4 font-semibold text-sm">
+                                            <h3 className="prim">${x.min_price} - ${x.max_price}</h3>
+                                            <button onClick={()=>handleDeleteProperty(x._id)} className="btn btn-sm text-red-600">X</button>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
                         </div>
                     </div>
                 </div>
