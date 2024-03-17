7890d7519f56805fde2d4d068d612d7636010a2c
sal400 <flarebd@gmail.com>
2024-03-17 19:29:15 +0600

    bought & sold properties fetched from offer requiest

--- src/pages/agent/ListingSold.jsx
+++ src/pages/agent/ListingSold.jsx
@@ -6,16 +6,15 @@ import { Link } from "react-router-dom";
 
 const ListingSold = () => {
     const { user } = useAuth()
-    const [listings, setListings] = useState([]);
-    const soldListings = listings.filter(x => x.property_holder !== user.email)
+    const [sold, setSold] = useState([]);
 
     //fetch data
     const filterEmail = user.email;
     useEffect(() => {
-        axiosBase(`/agent-listings?email=${filterEmail}`)
+        axiosBase(`/offer-sold?email=${filterEmail}`)
             .then(res => {
                 console.log(res.data);
-                setListings(res.data);
+                setSold(res.data);
             });
     }, [filterEmail]);
 
@@ -29,19 +28,19 @@ const ListingSold = () => {
                         <h3 className="text-2xl font-semibold mb-6 uppercase">Sold Properties</h3>
                         <div className="text-xl space-y-4">
                             <div>
-                                {soldListings.map((x, index) => (
+                                {sold.map((x, index) => (
                                     <div key={x._id} className="flex justify-between items-center my-4 md:px-10 mx-auto">
                                         <div className="md:flex items-center w-2/3">
                                             <div className="text-left">
-                                                <Link to={`/details/${x._id}`}>
+                                                <Link to={`/details/${x.propertyId}`}>
                                                     <h3 className="font-semibold text-sm">{index + 1}. {x.title}</h3>
                                                     <p className="text-sm">{x.location}</p>
-                                                    <p className="text-sm">Buyer: <span className="seco">{x.property_holder}</span></p>
+                                                    <p className="text-sm">Buyer: <span className="seco">{x.buyer_email}</span></p>
                                                 </Link>
                                             </div>
                                         </div>
                                         <div className="flex justify-end items-center gap-2 font-semibold text-sm">
-                                            <h3 className="prim text-right">${x.min_price} - ${x.max_price}</h3>
+                                            <h3 className="prim text-right">${x.offer_price}</h3>
                                         </div>
                                     </div>
                                 ))}
--- src/pages/user/Bought.jsx
+++ src/pages/user/Bought.jsx
@@ -1,12 +1,22 @@
 import { Link } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
-import useAxios from "../../hooks/useAxios";
 import UserDrawer from "./UserDrawer";
+import { useEffect, useState } from "react";
+import { axiosBase } from "../../hooks/useAxios";
 
 const Bought = () => {
     const { user } = useAuth()
-    const properties = useAxios('/properties')
-    const bought = properties.filter (x => x.property_holder === user.email)
+    const [bought, setBought] = useState([]);
+
+    //fetch data
+    const filterEmail = user.email;
+    useEffect(() => {
+        axiosBase(`/offer-bought?email=${filterEmail}`)
+            .then(res => {
+                console.log(res.data);
+                setBought(res.data);
+            });
+    }, [filterEmail]);
 
     return (
         <div className="md:flex border">
@@ -21,15 +31,15 @@ const Bought = () => {
                                     <div key={x._id} className="flex justify-between items-center my-4 md:px-10 mx-auto">
                                         <div className="md:flex items-center w-2/3">
                                             <div className="text-left">
-                                                <Link to={`/details/${x._id}`}>
+                                                <Link to={`/details/${x.propertyId}`}>
                                                     <h3 className="font-semibold text-sm">{index + 1}. {x.title}</h3>
                                                     <p className="text-sm">{x.location}</p>
-                                                    <p className="text-sm">Buyer: <span className="seco">{x.property_holder}</span></p>
+                                                    <p className="text-sm">Seller: <span className="seco">{x.agent_email}</span></p>
                                                 </Link>
                                             </div>
                                         </div>
                                         <div className="flex justify-end items-center gap-2 font-semibold text-sm">
-                                            <h3 className="prim text-right">${x.min_price} - ${x.max_price}</h3>
+                                            <h3 className="prim text-right">${x.offer_price}</h3>
                                         </div>
                                     </div>
                                 ))}
