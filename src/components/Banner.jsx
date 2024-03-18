8f7541d7fedb1450b495b6092257a5b090ade7ad
sal400 <flarebd@gmail.com>
2024-03-19 05:32:43 +0600

    banner mobile responsive issue fixt

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -21,7 +21,7 @@ const Banner = () => {
 
     return (
         <div>
-            <div className="rounded-lg lg:flex flex-col lg:justify-around lg:items-center bg-banner">
+            <div className="rounded-lg lg:flex flex-col lg:justify-around lg:items-center bg-banner overflow-hidden">
                 <div className="font-black leading-snug p-10 text-5xl md:text-6xl md:pt-20 md:leading-snug">
                     <h2 className="text-center">Swiss <span><ReactTyped strings={["Excellence", "Precision"]} typeSpeed={100} backSpeed={50} loop /></span> <br /> in Realty</h2>
                 </div>
